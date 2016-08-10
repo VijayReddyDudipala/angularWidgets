95531fe0520f562cc0d4b3d7252ded9f3a4963d4
Vijay Reddy Dudipala <vijaydudipala88@gmail.com>
2016-08-09 20:34:07 -0500

    InitiL PROJECT SETUP

---
+++ AngularWidgets/WebContent/js/app.js
@@ -0,0 +1,9 @@
+var app=angular.module("Widgets",["ngRoute"]);
+//Home Controller
+app.controller("homeController",["$scope", function($scope) {
+	$scope.title="Widgets";
+}]);
+//Flickr Controller
+app.controller("flickrController",["$scope",function($scope){
+	$scope.title="Flickr feed";
+}]);
